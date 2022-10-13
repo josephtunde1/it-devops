73d297b8d88b19c4741a62652a37042bb769f3da
josephtunde1 <ogu18003@byui.edu>
2022-10-13 08:52:58 -0600

    tuesday commit

---
+++ test/hello.test.js
@@ -0,0 +1,7 @@
+import hello from "./hello.js";
+import assert from "assert";
+
+it('Should print hello',()=>{
+    const helloResponse = hello();
+    assert.equal(helloResponse, 'hello');
+}); 
--- tests/hello.test.js
+++
@@ -1,3 +0,0 @@
-const {hello} =require('./hello.js')
-
-it('Should print hello',hello);
